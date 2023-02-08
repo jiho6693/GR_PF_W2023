0ad50c3bd972c50e939389699e9b51e19b0dd167
jiho6693 <56946702+jiho6693@users.noreply.github.com>
2023-02-07 20:01:46 -0500

    8:00 before clikc

--- index4.js
+++ index4.js
@@ -1,6 +1,7 @@
 import * as THREE from 'three';
 import { OrbitControls } from 'OrbitControls';
 import { OBJLoader } from 'OBJLoader';
+import { GLTFLoader } from 'GLTFLoader';
 import { DirectionalLight } from 'three';
 
 console.log(OrbitControls);
@@ -10,20 +11,32 @@ const scene = new THREE.Scene()
 scene.background = new THREE.Color(0xEEEEEE); 
 
 //땅
-const geometry = new THREE.PlaneGeometry(20,20 );
+// const geometry = new THREE.PlaneGeometry(20,20 );
+// const material = new THREE.MeshStandardMaterial({
+//   color:0xffffff,
+//   side: THREE.DoubleSide});
+// const ground = new THREE.Mesh(geometry, material);
+// ground.position.x= 0
+// ground.position.y= -1.5
+// ground.rotation.x = Math.PI / 2;
+// scene.add(ground);  
+// ground.receiveShadow = true;
+// ground.castShadow = true;
+
+const geometry = new THREE.PlaneGeometry(0.2,5 );
 const material = new THREE.MeshStandardMaterial({
   color:0xffffff,
   side: THREE.DoubleSide});
 const ground = new THREE.Mesh(geometry, material);
-ground.position.x= 0
-ground.position.y= -1.5
-ground.rotation.x = Math.PI / 2;
+ground.position.x= -2.5
+ground.position.y= 0
+ground.rotation.y = Math.PI / 2;
 scene.add(ground);  
 ground.receiveShadow = true;
 ground.castShadow = true;
 
 //벽
-const geometry01 = new THREE.PlaneGeometry(5,3 );
+const geometry01 = new THREE.PlaneGeometry(5,5 );
 const material01 = new THREE.MeshStandardMaterial({
   color:0xffffff,
   side: THREE.DoubleSide});
@@ -66,7 +79,7 @@ floor.castShadow = true;
 
 const ceil = new THREE.Mesh(geometry03, material01);
 ceil.position.x= 0
-ceil.position.y= +1.5
+ceil.position.y= +2.4
 ceil.rotation.x = Math.PI / 2;
 scene.add(ceil)
 ceil.castShadow = true;
@@ -76,53 +89,85 @@ ceil.receiveShadow = true;
 
 
 
+
+
 //obj
-const loader = new OBJLoader();
+const loader = new GLTFLoader();
 // // load a resource
 loader.load(
-// 	// resource URL
-	'Rock_1.obj',
-// 	// called when resource is loaded
-	function ( object ) {
-    object.scale.set(0.05,0.05,0.05);
-    object.position.y= -1
-     object.traverse( function ( child ){
-      
-     child.castShadow = true;
-     child.receiveShadow = true;
-    });
- 		scene.add( object );
-    // object.traverse( function ( child ){
-    // child.castShadow = true;
-    //  });
-    // obj.scene.traverse( function( child ) {
-    //   if (child.isMesh){ 
-    //    child.castShadow = true; 
-    //    child.receiveShadow = true; 
-    //   }
-    // });
-    
- 	},
+	// resource URL
+	'Rock1.glb',
+	// called when the resource is loaded
+	function ( gltf ) {
+    gltf.scene.scale.set(0.5, 0.5, 0.5); 
+    gltf.scene.position.y= -0.5
+    gltf.scene.position.z= -1.8
+    gltf.scene.position.x= 1
+    gltf.scene.traverse( function ( child ){
+      child.castShadow = true;
+      child.receiveShadow = true;
+     });
+		scene.add( gltf.scene );
 
-  
+		gltf.animations; // Array<THREE.AnimationClip>
+		gltf.scene; // THREE.Group
+		gltf.scenes; // Array<THREE.Group>
+		gltf.cameras; // Array<THREE.Camera>
+		gltf.asset; // Object
 
-// 	// called when loading is in progresses
- 	function ( xhr ) {
+	},
+	// called while loading is progressing
+	function ( xhr ) {
 
- 		console.log( ( xhr.loaded / xhr.total * 100 ) + '% loaded' );
+		console.log( ( xhr.loaded / xhr.total * 100 ) + '% loaded' );
 
- 	},
-// 	// called when loading has errors
- 	function ( error ) {
+	},
+	// called when loading has errors
+	function ( error ) {
 
- 		console.log( 'An error happened' );
+		console.log( 'An error happened' );
 
- 	}
- );
+	}
+);
 
-//텍스처 추가
-const textureLoader = new THREE.TextureLoader();
-const texture = textureLoader.load('Rock_1_Base_Color.jpg'); 
+const loader1 = new GLTFLoader();
+// // load a resource
+loader1.load(
+	// resource URL
+	'sca.glb',
+	// called when the resource is loaded
+	function ( sca ) {
+    sca.scene.scale.set(0.2, 0.2, 0.2); 
+    sca.scene.position.y= -1.5;
+    sca.scene.position.z= 1.1
+    sca.scene.position.x= -1
+    sca.scene.traverse( function ( child ){
+      child.castShadow = true;
+      child.receiveShadow = true;
+     });
+		scene.add( sca.scene );
+
+		sca.animations; // Array<THREE.AnimationClip>
+		sca.scene; // THREE.Group
+		sca.scenes; // Array<THREE.Group>
+		sca.cameras; // Array<THREE.Camera>
+		sca.asset; // Object
+
+	},
+	// called while loading is progressing
+	function ( xhr ) {
+
+		console.log( ( xhr.loaded / xhr.total * 100 ) + '% loaded' );
+
+	},
+	// called when loading has errors
+	function ( error ) {
+
+		console.log( 'An error happened' );
+
+	}
+);
+
 
  
 //카메라
@@ -137,7 +182,10 @@ scene.add(camera)
 
 //렌더러
 const canvas = document.querySelector('.webgl');
-const renderer = new THREE.WebGLRenderer();
+const renderer = new THREE.WebGLRenderer({
+  alpha : true,
+  antialias : true
+});
 renderer.shadowMap.enabled = true;
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
@@ -149,8 +197,8 @@ orbitControls.update();
 
 //빛
 
-const directionalLight = new THREE.DirectionalLight(0xFEF9E7 , 0.5);
-  directionalLight.position.set(-9, 5 , 0);
+const directionalLight = new THREE.DirectionalLight(0xFEF9E7 , 2);
+  directionalLight.position.set(-9, 0 , 2);
   const dlHelper = new THREE.DirectionalLightHelper
   (directionalLight, 0.2, 0x0000ff);
   scene.add(dlHelper);
@@ -159,14 +207,15 @@ const directionalLight = new THREE.DirectionalLight(0xFEF9E7 , 0.5);
   directionalLight.shadow.mapSize.width = 1024;
   directionalLight.shadow.mapSize.height  = 1024;
   directionalLight.shadow.radius = 1
+  directionalLight.shadow.bias = -0.0005;
 
-const light = new THREE.AmbientLight( 0x404040, 1.5); // soft white light
+const light = new THREE.AmbientLight( 0x404040, 0.5); // soft white light
 scene.add( light );
 
 
 function render(time) {
-time *= 0.00001;  // convert time to seconds  
-//directionalLight.position.y = Math.cos( time ) * 3.75 + 1.25;
+time *= 0.0001;  // convert time to seconds  
+directionalLight.position.y = Math.cos( time ) * 3.75 + 1.25;
 
 renderer.render(scene, camera);
 
